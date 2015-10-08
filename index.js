718b076fcd4edcf5d47904a4e0d648d0b3f8b834
Julian Gruber <julian@juliangruber.com>
2015-10-08 13:23:48 +0200

    expose .printable

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ Dump.prototype.hex = function(buf){
 Dump.prototype.strings = function(buf){
   var out = [];
   for (var i = 0; i < buf.length; i++) {
-    out.push(printable(buf[i])
+    out.push(this.printable(buf[i])
       ? String.fromCharCode(buf[i])
       : '.');
   }
@@ -33,7 +33,7 @@ Dump.prototype.lines = function(){
   return this._lines;
 };
 
-function printable(v){
+Dump.prototype.printable = function(v){
   return v >= 32 && v <= 126;
 }
 
